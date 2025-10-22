246013f37c4ec84cb09fb86d5d9e29e06c2d333d
Johan Mesa <Johanotr@gmail.com>
2025-10-21 22:33:42 -0400

    feat: handle cors and json

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 const routes = require("./routes");
 
-app.use("/`answer", routes.answer);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use("/answer", routes.answer);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
